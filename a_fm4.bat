8efcc41e94d979eb5faa22a426405d4ab181700a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-01 00:21:51 +0900

    W

--- a_fm4.bat
+++ a_fm4.bat
@@ -1,15 +1,14 @@
 call C:\WORKS\commands\general\works.bat
 call C:\WORKS\commands\general\WS_Arduino.bat
 call C:\WORKS\commands\general\WS_Cake_IFM10_PHP.bat
+call C:\WORKS\commands\general\WS_Electronics.bat
 
 REM a p && pushd C:\WORKS\WS\ADMIN_USB_1 && git add . && git commit -m "W" && git push origin master && pushd C:\WORKS\WS\Electronics && git add . && git commit -m "W" && git push origin master
 pause
 
 REM List of directories to git-push
-REM C:\WORKS
 REM C:\WORKS\WS\ADMIN_USB_1
-REM C:\WORKS\WS\Arduino
-REM C:\WORKS\WS\Cake\IFM10_PHP
+
 REM C:\WORKS\WS\Cake\IFM_CAKE_1
 REM C:\WORKS\WS\Electronics
 REM C:\WORKS\WS\G_nbp_new
---
+++ commands/general/WS_Electronics.bat
@@ -0,0 +1,1 @@
+C:\WORKS\a p && cd C:\WORKS\WS\Electronics && git add . && git commit -m "W" && git push origin master
