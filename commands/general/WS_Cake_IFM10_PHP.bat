01e2d3ee41ad02f2f62830f52853c4441922025e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-01 00:10:34 +0900

    W

--- commands/general/WS_Cake_IFM10_PHP.bat
+++ commands/general/WS_Cake_IFM10_PHP.bat
@@ -1,13 +1,1 @@
-a p && pushd C:\WORKS\WS\ADMIN_USB_1 && git add . && git commit -m "W" && git push origin master && pushd C:\WORKS\WS\Electronics && git add . && git commit -m "W" && git push origin master
-pause
-
-REM List of directories to git-push
-REM C:\WORKS
-REM C:\WORKS\WS\ADMIN_USB_1
-REM C:\WORKS\WS\Arduino
-REM C:\WORKS\WS\Cake\IFM10_PHP
-REM C:\WORKS\WS\Cake\IFM_CAKE_1
-REM C:\WORKS\WS\Electronics
-REM C:\WORKS\WS\G_nbp_new
-REM C:\WORKS\WS
-REM 
+C:\WORKS\a p && cd C:\WORKS\WS\Cake\IFM10_PHP && git add . && git commit -m "W" && git push origin master
