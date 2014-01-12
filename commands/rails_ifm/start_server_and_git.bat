36bf134014fbd21fd04476dc92854fa7af95e363
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-13 00:22:52 +0900

    W

--- commands/rails_ifm/start_server_and_git.bat
+++ commands/rails_ifm/start_server_and_git.bat
@@ -1,3 +1,4 @@
 pushd C:\WORKS
-a p && pushd C:\WORKS\WS\WS_Android_20130619_090924\IFM_R_1 && gitk && rails s --port 3001
+REM a p && pushd C:\WORKS\WS\WS_Android_20130619_090924\IFM_R_1 && gitk && rails s --port 3001
+a p && pushd C:\WORKS\WS\WS_Android\IFM_R_1 && gitk && rails s --port 3001
 pause
