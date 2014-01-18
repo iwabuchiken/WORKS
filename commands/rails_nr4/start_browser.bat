c0959941e21a7d2d0a262ec8ed1f3a4ee0c5d7ba
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-19 00:13:25 +0900

    W

--- commands/rails_nr4/start_browser.bat
+++ commands/rails_nr4/start_browser.bat
@@ -1,4 +1,8 @@
 pushd "C:\Program Files\Google\Chrome\Application"
 
+REM echo Waiting for 15 seconds...
+REM REF http://stackoverflow.com/questions/1672338/how-to-sleep-for-5-seconds-in-windowss-command-prompt-or-dos answered Nov 4 '09 at 8:20
+REM ping 1.1.1.1 -n 1 -w 15000 > nul
+
 REM REF http://superuser.com/questions/153245/chrome-command-line-arguments-w-url asked Jun 16 '10 at 15:22
 start chrome.exe http://localhost:3001/nr4/articles
--- commands/rails_nr4/start_server_and_git.bat
+++ commands/rails_nr4/start_server_and_git.bat
@@ -1,2 +1,12 @@
 pushd C:\WORKS
+
+REM REF http://stackoverflow.com/questions/3022176/how-to-verify-if-a-file-exists-in-a-windows-bat-file answered Jun 11 '10 at 11:05
+
+IF EXIST C:\WORKS\WS\WS_Android_20130929_122122\JQM-1\tmp\pids\server.pid (
+	
+	echo server.pid exists. Deleting...
+	del C:\WORKS\WS\WS_Android_20130929_122122\JQM-1\tmp\pids\server.pid
+)
+
 a p && pushd C:\WORKS\WS\WS_Android_20130929_122122\JQM-1 && gitk && rails s --port 3001
+pause
