985e1808f4cc5b2cc24180b1314500c7edf8d6e1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-03-09 23:28:15 +0900

    W

---
+++ commands/and_app.twitter/main.bat
@@ -0,0 +1,9 @@
+call C:\WORKS\commands\and_app.twitter\start_eclipse.bat
+
+call C:\WORKS\commands\and_app.twitter\start_prompt.bat
+
+call C:\WORKS\commands\and_app.twitter\start_server_and_git.bat
+
+REM start C:\WORKS\PROGRAMS\pleiades-e4.3-ultimate-32bit-jre_20130617\pleiades\eclipse\eclipse.exe
+REM pause
+REM a p && cd C:\WORKS\WS\WS_Android~\FM && git add . && git commit -m "W" && git push origin master
