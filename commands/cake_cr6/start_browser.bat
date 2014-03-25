bbd142dbb1dae9eb01e812113a999f11245ef48d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-03-25 23:46:22 +0900

    W

--- commands/cake_cr6/start_browser.bat
+++ commands/cake_cr6/start_browser.bat
@@ -6,4 +6,4 @@ REM ping 1.1.1.1 -n 1 -w 15000 > nul
 
 REM REF http://superuser.com/questions/153245/chrome-command-line-arguments-w-url asked Jun 16 '10 at 15:22
 REM start chrome.exe http://localhost
-start chrome.exe http://localhost/PHP_server/CR6_cake/ http://benfranklin.chips.jp/cake_apps/CR6_cake/texts/index
+start chrome.exe http://localhost/PHP_server/CR6_cake/ http://benfranklin.chips.jp/cake_apps/CR6_cake/texts/index https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cr6cake&server=108&token=cf10dc9e5a6fc4e9baf63cf124203166
