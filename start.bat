ac59c9603778fea1aaeedd7ad6d82fe4424c3337
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-05-06 01:37:02 +0900

    W

--- start.bat
+++ start.bat
@@ -2,6 +2,7 @@ REM C:\WORKS\PROGRAMS\Freemind_1.0.0\FreeMind\Freemind.exe
 C:\WORKS\PROGRAMS\Freemind_1.0.0\FreeMind\Freemind.exe
 
 start C:\WORKS
+start C:\Users\IwabuchiK\Desktop\shortcuts
 
 start C:\WORKS\"Command Prompt.lnk"
 
