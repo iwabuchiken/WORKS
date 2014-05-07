bd474dc0c982f7977b43a2e879298c352630a8b6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-05-08 00:43:52 +0900

    W

--- commands/image_processing/start_eclipse.bat
+++ commands/image_processing/start_eclipse.bat
@@ -1,2 +1,6 @@
 pushd C:\WORKS\PROGRAMS\adt-bundle-windows-x86-20130917\adt-bundle-windows-x86-20130917\eclipse
 start eclipse.exe
+
+pushd C:\WORKS\PROGRAMS\Netbeans_8_0\bin
+# REF B option http://bytes.com/topic/c-sharp/answers/267083-why-windows-doesnt-allow-launch-daemon-service-command-line#post1070029
+start /B netbeans.exe
