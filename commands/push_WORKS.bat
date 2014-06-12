8e7224a7dff3d86afeca726114d95e3f377277a6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-12 23:40:29 +0900

    W

--- commands/push_WORKS.bat
+++ commands/push_WORKS.bat
@@ -4,10 +4,12 @@ REM set abc=%PUSH%
 
 if %PUSH%=="yes" (
 
+	echo PUSH is set
 	cd C:\WORKS && git add . && git commit -m "W" && git push origin master
 
 ) else (
 
+	echo PUSH is not set
 	C:\WORKS\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
 	
 )
