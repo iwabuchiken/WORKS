e998067cebf9781913fad4077eec8d5ccd1729b3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-12 09:51:06 +0900

    W

---
+++ shortcuts/emb/main.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+start C:\WORKS\shortcuts\emb\start_command_prompot
+
+start start_apps
+
+start_git
+
+REM start start_git_lib
+
+exit
---
+++ shortcuts/emb/start_apps.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+pushd C:\WORKS\Programs\Mikroelectronika\mikroC
+start mikroC.exe
+
+pushd C:\WORKS\Programs\Microchip\PICkit 2 v2
+start PICkit2V2.exe
+
+pushd C:\Program Files\Pico Technology\PicoScope6
+start PicoScope.exe
+
+exit
