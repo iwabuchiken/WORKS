41ad71ac23f3565180cb88431b775b1d996ab548
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-12 23:29:14 +0900

    W

--- commands/push_WORKS.bat
+++ commands/push_WORKS.bat
@@ -1,4 +1,16 @@
 echo cd C:\WORKS
 
+set abc=%PUSH%
+
+if abc=="yes" (
+
+	echo PUSH
+
+) else (
+
+	echo NOT PUSH
+	
+)
+
 C:\WORKS\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
 
