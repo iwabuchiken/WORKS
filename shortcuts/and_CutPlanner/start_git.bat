8fccd33ef8019e56c0e95bd32bdd8f928c91b9aa
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-17 01:07:37 +0900

    W

---
+++ shortcuts/and_CutPlanner/start_git.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W && a && gitk
+REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W
+REM gitk
+
+REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W && gitk
+REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W && C:\WORKS\Programs\Git\cmd\gitk
+pushd C:\WORKS\WS\Eclipse_ADT\cm7 && C:\WORKS\Programs\Git\cmd\gitk
+
+REM pause 
+
+exit
