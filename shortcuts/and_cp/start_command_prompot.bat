94fed4bc91882cc801e9a940fc275f8726c1eb39
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-20 00:33:52 +0900

    W

---
+++ shortcuts/and_cp/start_command_prompot.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+start C:\WORKS\shortcuts\and_cp\"Command Prompt.lnk"
+
+exit
--- shortcuts/cake_cr6/main.bat
+++ shortcuts/cake_cr6/main.bat
@@ -5,6 +5,8 @@ start C:\WORKS\shortcuts\cake_cr6\start_command_prompot
 REM start "start_eclipse_kepler,filezilla,xampp.bat"
 start start_eclipse_kepler-filezilla-xampp
 
+REM start_chrome
+
 start_git
 
 REM start start_git_lib
