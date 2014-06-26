c00635f5e7c9e144fa3184a7d2b859bee030c37d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-06-26 22:01:26 +0900

    W

---
+++ shortcuts/and_lm1/main.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+start C:\WORKS\shortcuts\and_lm1\start_command_prompot
+
+start start_eclipse_kepler
+
+start_git
+
+exit
---
+++ shortcuts/and_lm1/start_command_prompot.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+start C:\WORKS\shortcuts\and_lm1\"Command Prompt.lnk"
+
+exit
