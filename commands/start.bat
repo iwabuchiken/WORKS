750651abe7b929c9d16785d8a0137f208193b446
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-07-26 16:56:33 +0900

    W

--- commands/start.bat
+++ commands/start.bat
@@ -6,7 +6,9 @@ pushd "C:\Program Files\Google\Chrome\Application"
 start chrome.exe
 
 
-pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
+REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
+pushd C:\WORKS\Programs\FreeMind_1.0.0
+REM pushd C:\WORKS\Programs\FreeMind_1.0.0\FreeMind
 start FreeMind
 
 
