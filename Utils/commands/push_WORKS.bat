fc09c235e20b86b02ad2e2238a5d3a064b4f927d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-08-14 21:36:46 +0900

    W

--- Utils/commands/push_WORKS.bat
+++ Utils/commands/push_WORKS.bat
@@ -2,5 +2,8 @@
 
 echo cd C:\WORKS -------------------
 
-C:\WORKS\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
+
+
+REM C:\WORKS\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
+C:\WORKS\a && cd C:\WORKS && git add -A && git commit -m "W" && git push origin master
 	
