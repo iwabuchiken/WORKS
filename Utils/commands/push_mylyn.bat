d6999d732a3fa4bdfc5ed9a336a007981693d74b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-09-04 22:51:29 +0900

    W

--- Utils/commands/push_mylyn.bat
+++ Utils/commands/push_mylyn.bat
@@ -6,4 +6,5 @@ echo cd C:\WORKS\WS\Eclipse_ADT\.metadata -------------------
 
 REM C:\WORKS\a && cd C:\WORKS && git add . && git commit -m "W" && git push origin master
 C:\WORKS\a && cd C:\WORKS\WS\Eclipse_ADT\.metadata && git add -A && git commit -m "W" && git push origin master
-	
+
+C:\WORKS\a && cd C:\WORKS\WS\Eclipse_Kepler\.metadata && git add -A && git commit -m "W" && git push origin master	
---
+++ Utils/shortcuts/art_video/start_apps.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+REM start "C:\Program Files (x86)\Windows Live\Photo Gallery\MovieMaker.exe"
+pushd "C:\Program Files (x86)\Windows Live\Photo Gallery"
+start MovieMaker.exe
+
+pushd "C:\Program Files (x86)\NCH Software\VideoPad"
+start videopad.exe
+
+exit
