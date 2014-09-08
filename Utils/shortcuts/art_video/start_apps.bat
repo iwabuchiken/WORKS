7f2145141f2f663e926b6df2eb97815e65747039
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-09-09 00:05:24 +0900

    W

--- Utils/shortcuts/art_video/start_apps.bat
+++ Utils/shortcuts/art_video/start_apps.bat
@@ -4,7 +4,9 @@ REM start "C:\Program Files (x86)\Windows Live\Photo Gallery\MovieMaker.exe"
 pushd "C:\Program Files (x86)\Windows Live\Photo Gallery"
 start MovieMaker.exe
 
-pushd "C:\Program Files (x86)\NCH Software\VideoPad"
-start videopad.exe
+pushd "C:\WORKS\Programs\GIMP_2.8.10\bin"
+start gimp-2.8
+
+
 
 exit
