eb798a61b3184b15ca86ff1140b7114495a27500
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-09-18 22:33:05 +0900

    W

--- Utils/shortcuts/cake_nr5/start_git.bat
+++ Utils/shortcuts/cake_nr5/start_git.bat
@@ -1,13 +1,9 @@
 @ECHO OFF
 
-REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W_J && C:\WORKS\Programs\Git\cmd\gitk
+pushd C:\WORKS\WS\Eclipse_Kepler\Cake_NR5
 
-pushd C:\WORKS\WS\Cake\Cake_NR5
+REM pushd C:\WORKS\WS\Cake\Cake_NR5
 
 gitk
-
-REM C:\WORKS\Programs\Git\cmd\gitk
-
-REM pause 
 
 exit
