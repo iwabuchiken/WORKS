c3f3baab8a5562074a283457a1ec0367dadd6467
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-20 23:57:18 +0900

    W

--- Utils/shortcuts/cake_vm/start_git.bat
+++ Utils/shortcuts/cake_vm/start_git.bat
@@ -2,7 +2,7 @@
 
 REM pushd C:\WORKS\WS\Eclipse_Kepler\Learn_Sound_Eclipse_W_J && C:\WORKS\Programs\Git\cmd\gitk
 
-pushd C:\WORKS\WS\Cake\VM_Cake
+pushd C:\WORKS\WS\Eclipse_Kepler\VM_Cake
 C:\WORKS\Programs\Git\cmd\gitk
 
 REM pause 
