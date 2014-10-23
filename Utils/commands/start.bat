28a04df38dff4b8423bc36d677e9edc17300084c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-23 23:31:19 +0900

    W

--- Utils/commands/start.bat
+++ Utils/commands/start.bat
@@ -3,8 +3,10 @@ REM start "Command Prompt.lnk"
 start C:\WORKS\"Command Prompt.lnk"
 
 REM pushd "C:\Program Files\Google\Chrome\Application"
-pushd "C:\Program Files (x86)\Google\Chrome\Application"
-start chrome.exe
+REM pushd "C:\Program Files (x86)\Google\Chrome\Application"
+pushd C:\WORKS\Programs\Firefox_33.0
+start firefox
+REM start chrome.exe
 
 
 REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
---
+++ Utils/shortcuts/cake_cr6/start_command_prompot.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+start C:\WORKS\Utils\shortcuts\cake_cr6\"Command Prompt.lnk"
+
+
+exit
