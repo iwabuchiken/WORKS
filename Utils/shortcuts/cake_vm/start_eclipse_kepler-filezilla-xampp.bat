2adf984d74afb1ce9b92e5182e3013fc54c383ab
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-11-11 00:24:30 +0900

    W

--- Utils/shortcuts/cake_vm/start_eclipse_kepler-filezilla-xampp.bat
+++ Utils/shortcuts/cake_vm/start_eclipse_kepler-filezilla-xampp.bat
@@ -3,10 +3,10 @@
 pushd C:\WORKS\Programs\eclipse_kepler
 start eclipse.exe
 
-pushd C:\WORKS\Programs\xampp_1.8.3
+pushd C:\WORKS\Programs\xampp_1.8.2_x86
 start xampp-control.exe
 
-pushd C:\WORKS\Programs\FileZilla_3.9.0.2\FileZilla_FTP_Client
+pushd C:\WORKS\Programs\FileZilla\FileZilla_3.9.0.5\FileZilla FTP Client
 start filezilla.exe
 
 
