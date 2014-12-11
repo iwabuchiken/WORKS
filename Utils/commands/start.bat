5f8fd8c188ae13e775608758b4d3ae98378f43d4
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-11 23:58:10 +0900

    W

--- Utils/commands/start.bat
+++ Utils/commands/start.bat
@@ -1,16 +1,10 @@
 REM pushd C:\WORKS
 REM start "Command Prompt.lnk"
-start C:\WORKS\"Command Prompt.lnk"
+REM start C:\WORKS\"Command Prompt.lnk"
 
 REM pushd "C:\Program Files\Google\Chrome\Application"
 REM pushd C:\WORKS\Programs\Firefox_33.0
 REM start firefox
-
-pushd "C:\Program Files (x86)\Google\Chrome\Application"
-start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
-start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
-start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
-
 
 REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
 pushd C:\WORKS\Programs\FreeMind_1.0.1
@@ -20,4 +14,12 @@ start FreeMind
 
 start C:\WORKS
 
+
+
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
+
+REM timeout /t 10
 
