265f91316a8e1eb26e258864e6312555c0e86776
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-15 23:50:44 +0900

    W

--- Utils/commands/start.bat
+++ Utils/commands/start.bat
@@ -18,10 +18,10 @@ start C:\WORKS
 timeout /t 3
 
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
-chrome.exe
-REM start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
-REM start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
-REM start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
+REM chrome.exe
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
+start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
 
 REM timeout /t 10
 
