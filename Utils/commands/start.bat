07888e6da518ccc118e0cae0753bd393ac6c2f23
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-17 23:22:31 +0900

    W

--- Utils/commands/start.bat
+++ Utils/commands/start.bat
@@ -7,21 +7,26 @@ REM pushd C:\WORKS\Programs\Firefox_33.0
 REM start firefox
 
 REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
-pushd C:\WORKS\Programs\FreeMind_1.0.1
+REM pushd C:\WORKS\Programs\FreeMind_1.0.1
 REM pushd C:\WORKS\Programs\FreeMind_1.0.0\FreeMind
-start FreeMind
+REM start FreeMind
 
 
 start C:\WORKS
 
 
-timeout /t 3
+timeout /t 2
 
 pushd "C:\Program Files (x86)\Google\Chrome\Application"
 REM chrome.exe
-start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
 start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
 start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
+start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
 
-REM timeout /t 10
+timeout /t 5
+
+REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
+pushd C:\WORKS\Programs\FreeMind_1.0.1
+REM pushd C:\WORKS\Programs\FreeMind_1.0.0\FreeMind
+start FreeMind
 
