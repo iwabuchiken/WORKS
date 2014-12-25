c96cf0394ba905f52bb25e0f0c495ca2f1d43657
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-26 01:13:09 +0900

    W

--- Utils/commands/start.bat
+++ Utils/commands/start.bat
@@ -23,7 +23,8 @@ start chrome.exe http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
 start chrome.exe https://www.google.com/calendar/render?tab=ic#g%7Cmonth-3+22845+22882+22874
 start chrome.exe http://benfranklin.chips.jp/cake_apps/Cake_NR5/articles?genre_id=3
 
-timeout /t 5
+REM timeout /t 5
+timeout /t 8
 
 REM pushd C:\WORKS\Programs\Freemind_1.0.1\FreeMind
 pushd C:\WORKS\Programs\FreeMind_1.0.1
